0da6f6532f63886860c49c7ae09df8f2b1d59f15
KykharchikLiza <71406123+KykharchikLiza@users.noreply.github.com>
2021-12-09 23:58:19 +0300

    Add files via upload

---
+++ laba_1/ThreadPool/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPool
+{
+    delegate void TaskDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(7); //создает указанное количество потоков пула
+
+            taskQueue.EnqueueTask(One);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(Two);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(Three);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(One);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(Two);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(Three);
+            Thread.Sleep(100);
+
+           /* taskQueue.EnqueueTask(One);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(Two);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(Three);
+            Thread.Sleep(100);*/
+
+            Console.ReadLine();
+        }
+
+        private static void One()
+        {
+            Console.WriteLine($"TaskOne - {Thread.CurrentThread.Name}");
+        }
+        private static void Two()
+        {
+            Console.WriteLine($"TaskTwo - {Thread.CurrentThread.Name}");
+        }
+        private static void Three()
+        {
+            Console.WriteLine($"TaskThree - {Thread.CurrentThread.Name}");
+        }
+    }
+}
---
+++ laba_1/ThreadPool/TaskQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class TaskQueue
+    {
+        private Queue<Thread> threadPool;
+        private TaskDelegate taskDelegate;
+
+        public TaskQueue(int count)
+        {
+            threadPool = new Queue<Thread>();
+            InitPool(count);
+        }
+
+        private void InitPool(int count)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Thread myThread = new Thread(new ThreadStart(ThreadProc));//создаем новый поток
+                myThread.Name = $"{i}";//имя потока
+                myThread.Start(); //запускаем поток
+                threadPool.Enqueue(myThread); //добавляем в конец очереди
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task) //постановка в очередь и послед выполнение
+        {
+            lock (this)
+            {
+                taskDelegate += task;
+            }
+        }
+
+        private void ThreadProc()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (taskDelegate != null)
+                    {
+                        taskDelegate();
+                        taskDelegate = null;
+                    }
+                }
+            }
+        }
+    }
+}
