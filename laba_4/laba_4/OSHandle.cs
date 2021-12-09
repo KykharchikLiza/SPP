a164599011d3eb3cca08db013a4b7821c8696389
KykharchikLiza <71406123+KykharchikLiza@users.noreply.github.com>
2021-12-10 00:03:13 +0300

    Add files via upload

---
+++ laba_4/laba_4/OSHandle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MPP_lab4
+{
+    public class OSHandle:IDisposable
+    {
+        [DllImport("Kernel32.dll")]
+        //закрывает дескриптор открытого объекта
+        private static extern bool CloseHandle(IntPtr handle);
+        private IntPtr Handle { get; set; }
+        
+        private bool _disposed;
+
+        public OSHandle(IntPtr handle)
+        {
+            Handle = handle;
+        }
+        // реализация интерфейса IDisposable
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                CloseHandle(Handle);
+                Handle = IntPtr.Zero;
+                _disposed = true;
+            }
+            // подавляем финализацию
+            GC.SuppressFinalize(this);
+        }
+        //деструктор
+        ~OSHandle()
+        {
+            Dispose ();
+        }
+    }
+}
---
+++ laba_4/laba_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MPP_lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int handle = 0;
+            var osHandle = new OSHandle(new IntPtr(handle));
+        }
+    }
+}
