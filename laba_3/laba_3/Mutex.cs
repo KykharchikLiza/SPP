7949dd7d156208e9f0f748127e8bfdad560310a1
KykharchikLiza <71406123+KykharchikLiza@users.noreply.github.com>
2021-12-10 00:00:25 +0300

    Add files via upload

---
+++ laba_3/laba_3/Mutex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    class Mutex
+    {
+        private int semaphore;
+
+        public Mutex()
+        {
+            semaphore = 0;
+        }
+
+        public void Lock()
+        {
+            while(Interlocked.CompareExchange(ref semaphore, Thread.CurrentThread.ManagedThreadId, 0) != 0)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public void Unlock()
+        {
+            Interlocked.CompareExchange(ref semaphore, 0, Thread.CurrentThread.ManagedThreadId);
+        }
+        
+    }
+}
---
+++ laba_3/laba_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    class Program
+    {
+
+        private static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+           
+            for(int i = 0; i < 6; i++)
+            {
+                Thread myThread = new Thread(proc);
+                myThread.Start();
+            }
+            Console.ReadLine();
+        }
+
+        public static void proc()
+        {
+            mutex.Lock();
+            Console.WriteLine("Start thread " + Thread.CurrentThread.ManagedThreadId);
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("thread " + Thread.CurrentThread.ManagedThreadId + " step " + i);
+            }
+            Console.WriteLine("Finish thread " + Thread.CurrentThread.ManagedThreadId);
+            mutex.Unlock();
+        }
+    }
+}
